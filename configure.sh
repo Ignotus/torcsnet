8d4e9ebb4bc676ae7b1bc781e7d3086fa864ca20
Minh Ngo <nlminhtl@gmail.com>
2015-11-02 11:45:01 +0100

    Fixing a bug with libpng

--- configure.sh
+++ configure.sh
@@ -14,6 +14,8 @@ tar -xvxf torcs-1.3.1.tar.bz2
 mv champ2010patch.tgz torcs-1.3.1/
 pushd torcs-1.3.1
   ./configure
+  sed -i 's/-lpng/-lpng12/g' Make-config
+  sed -i 's/#include "png\.h"/#include <libpng12\/png.h>/' src/libs/tgfclient/img.cpp
   #tar -xvxf champ2010patch.tgz
   #cd ./src/ && patch -R -p1 < ../champ2010patch/patch.dat
   #cd ..
