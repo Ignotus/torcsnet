3e3a5b1324c720904233d686997773b816b81fd5
Sander Lijbrink <sander.lijbrink@student.uva.nl>
2015-11-24 16:20:29 +0100

    Finished MLP impl in Java

--- src/main/java/MLPNN.java
+++ src/main/java/MLPNN.java
@@ -1,12 +1,17 @@
+import org.apache.commons.math3.analysis.function.Sigmoid;
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created by sander on 23/11/15.
+ * Multilayer Perceptron implementation with one hidden layer
  */
 public class MLPNN {
-    private double mLearningRate;
+    private static final Sigmoid sigmoid = new Sigmoid();
 
     // Layers sizes
     private int mInputLayerSize;
@@ -14,27 +19,149 @@ public class MLPNN {
     private int mOutputLayerSize;
 
     // Layer matrices
-    private ArrayRealVector mInputLayer;
-    private ArrayRealVector mHiddenLayer;
-    private ArrayRealVector mOutputLayer;
+    private RealVector mInputLayer;
+    private RealVector mHiddenLayer;
+    private RealVector mOutputLayer;
 
     // Weights between input and hidden layer
-    private Array2DRowRealMatrix mW1;
+    private RealMatrix mW1;
     // Weights between hidden and output layer
-    private Array2DRowRealMatrix mW2;
+    private RealMatrix mW2;
+
+    // Stored vectors, used in backpropagation, to minimize allocations
+    private RealVector mOutputLayerGradients;
+    private RealVector mHiddenLayerGradients;
 
     public MLPNN(int inputLayerSize, int hiddenLayerSize, int outputLayerSize) {
         this.mInputLayerSize = inputLayerSize;
         this.mHiddenLayerSize = hiddenLayerSize;
         this.mOutputLayerSize = outputLayerSize;
+
+        // +1 for bias weight
+        this.mInputLayer = new ArrayRealVector(inputLayerSize + 1);
+        this.mHiddenLayer = new ArrayRealVector(hiddenLayerSize + 1);
+        this.mOutputLayer = new ArrayRealVector(outputLayerSize + 1);
+
+        this.mW1 = new Array2DRowRealMatrix(hiddenLayerSize + 1, inputLayerSize + 1);
+        this.mW2 = new Array2DRowRealMatrix(outputLayerSize + 1, hiddenLayerSize + 1);
+
+        this.mOutputLayerGradients = new ArrayRealVector(outputLayerSize + 1);
+        this.mHiddenLayerGradients = new ArrayRealVector(hiddenLayerSize + 1);
+
         initializeWeights();
     }
 
     private void initializeWeights() {
-        // Initialize W1 and W2 with random values
-        // Uniformly sampled from the an interval
-        double max = 4f * Math.sqrt(6f / (mInputLayerSize + mOutputLayerSize));
-        double min = -4f * Math.sqrt(6f / (mInputLayerSize + mOutputLayerSize));;
+        // Initialize W1 and W2 with values uniformly sampled from an interval
+        double max = 0.5;
+        double min = -0.5;
 
+        ThreadLocalRandom rand = ThreadLocalRandom.current();
+        for (int i = 1; i <= mInputLayerSize; i++) {
+            for (int j = 0; j <=  mHiddenLayerSize; j++) {
+                mW1.setEntry(j, i, rand.nextDouble(min, max));
+            }
+        }
+
+        for (int i = 1; i <= mHiddenLayerSize; i++) {
+            for (int j = 0; j <= mOutputLayerSize; j++) {
+                mW2.setEntry(j, i, rand.nextDouble(min, max));
+            }
+        }
     }
+
+    // W1 : weights between input and hidden layer
+    // W2 : weights between hidden and output layer
+    public void setWeights(RealMatrix W1, RealMatrix W2) {
+        this.mW1 = W1;
+        this.mW2 = W2;
+    }
+
+    public void pass(RealVector input) {
+        // Set bias
+        mInputLayer.setEntry(0, 1.0);
+        mHiddenLayer.setEntry(0, 1.0);
+
+        // Fill input layer
+        mInputLayer.setSubVector(1, input);
+
+        // Pass data through the network
+        double sum;
+        for (int i = 1; i <= mHiddenLayerSize; i++) {
+            // Input to hidden layer
+            sum = 0.0;
+            for (int j = 0; j <= mInputLayerSize; j++) {
+                sum += mW1.getEntry(i, j) * mInputLayer.getEntry(j);
+            }
+            mHiddenLayer.setEntry(i, sigmoid.value(sum));
+        }
+
+        for (int i = 1; i <= mOutputLayerSize; i++) {
+            // Hidden to output layer
+            sum = 0.0;
+            for (int j = 0; j <= mHiddenLayerSize; j++) {
+                sum += mW2.getEntry(i, j) * mHiddenLayer.getEntry(j);
+            }
+            mOutputLayer.setEntry(i, sigmoid.value(sum));
+        }
+    }
+
+    public RealVector predict(RealVector input) {
+        pass(input);
+        return mOutputLayer;
+    }
+
+    // input: vector of length mInputLayerSize
+    // target: vector of length mOutputLayerSize
+    public void train(RealMatrix data, RealMatrix target, int numIterations, double learningRate) {
+        for (int i = 0; i < numIterations; i++) {
+            for (int row = 0; row < data.getRowDimension(); row++) {
+                pass(data.getRowVector(row));
+                propagateError(target.getRowVector(row), learningRate);
+            }
+        }
+    }
+
+    // https://en.wikipedia.org/wiki/Multilayer_perceptron
+    // Updates the weights through back propagation
+    // Uses gradient descent
+    private void propagateError(RealVector target, double learningRate) {
+        // Re-use vectors to minimize allocations
+        mOutputLayerGradients.set(0);
+        mHiddenLayerGradients.set(0);
+
+        /* Compute output layer gradients */
+        for (int i = 1; i <= mOutputLayerSize; i++) {
+            double output = mOutputLayer.getEntry(i);
+            double error = target.getEntry(i - 1) - output;
+            mOutputLayerGradients.setEntry(i, error * output * (1.0 - output));
+        }
+
+        /* Compute hidden layer gradients */
+        for (int i = 0; i <= mHiddenLayerSize; i++) {
+            double errorSum = 0.0;
+            for (int j = 1; j <= mOutputLayerSize; j++) {
+                errorSum += mW2.getEntry(j, i) * mOutputLayerGradients.getEntry(j);
+            }
+            double output = mHiddenLayer.getEntry(i);
+            mHiddenLayerGradients.setEntry(i, errorSum * output * (1.0 - output));
+        }
+
+        /* Update output layer weights (gradient descent) */
+        for (int i = 1; i <= mOutputLayerSize; i++) {
+            double grad = mOutputLayerGradients.getEntry(i);
+            for (int j = 0; j <= mHiddenLayerSize; j++) {
+                mW2.addToEntry(i, j, learningRate * grad * mHiddenLayer.getEntry(j));
+            }
+        }
+
+        /* Update hidden layer weights (gradient descent) */
+        for (int i = 1; i <= mHiddenLayerSize; i++) {
+            double grad = mHiddenLayerGradients.getEntry(i);
+            for (int j = 0; j <= mInputLayerSize; j++) {
+                mW1.addToEntry(i, j, learningRate * grad * mInputLayer.getEntry(j));
+            }
+        }
+    }
+
 }
--- src/test/java/MLPNNTest.java
+++ src/test/java/MLPNNTest.java
@@ -1,57 +1,57 @@
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
-import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.util.MathUtils;
 import org.junit.Test;
 import org.knowm.xchart.Chart;
 import org.knowm.xchart.ChartBuilder;
 import org.knowm.xchart.StyleManager;
-import scala.Tuple2;
 
 /**
- * Created by sander on 17/11/15.
+ * Created by sander on 24/11/15.
  */
-public class MLPTest {
-
+public class MLPNNTest {
     @Test
     public void testTrainingAndPrediction() {
-        System.out.println("MLPTest started");
+        System.out.println("MLPNN Test started");
 
         /* Generate sinusoidal input data */
         RealVector x = Utils.linspace(0, MathUtils.TWO_PI, 100);
         RealMatrix X = new Array2DRowRealMatrix(x.getDimension(), 1);
         RealVector y = x.map(v -> Math.sin(v) / 2 + 0.5);
-        for (int i = 0; i < x.getDimension(); ++i) {
+        RealMatrix Y = new Array2DRowRealMatrix(y.getDimension(), 1);
+        for (int i = 0; i < x.getDimension(); i++) {
             X.setRow(i, new double[]{x.getEntry(i)});
         }
+        for (int i = 0; i < y.getDimension(); i++) {
+            Y.setRow(i, new double[]{y.getEntry(i)});
+        }
 
-        MLPSetup setup = new MLPSetup(1, 10);
-        MLP nn = new MLP(setup);
+        MLPNN nn = new MLPNN(1, 10, 1);
 
         long start = System.currentTimeMillis();
-        nn.train(X, y, 100, 0.1);
-        System.out.println("MLPTest: Training took " + (System.currentTimeMillis() - start) + "ms");
+        // Train for 100 iterations
+        nn.train(X, Y, 100, 0.1);
+        System.out.println("MLPNNTest: Training took " + (System.currentTimeMillis() - start) + "ms");
 
-       /* Get predictions */
+        /* Get predictions */
         double squaredError = 0.0;
         double[] yPredicted = new double[y.getDimension()];
-
         start = System.currentTimeMillis();
         for (int i = 0; i < x.getDimension(); i++) {
-            yPredicted[i] =nn.predict(X.getRowVector(i));
+            RealVector pred = nn.predict(X.getRowVector(i));
+            yPredicted[i] = pred.getEntry(1);
             squaredError += Math.pow((yPredicted[i] - y.getEntry(i)), 2);
         }
-        System.out.println("MLPTest Predicting took " + (System.currentTimeMillis() - start) + "ms");
-        System.out.println("MLPTest squared error: " + squaredError);
+        System.out.println("MLPNNTest: squared error: " + squaredError);
+        System.out.println("MLPNNTest: Predicting took " + (System.currentTimeMillis() - start) + "ms");
 
         /* Save results to chart */
         double[] xplot = x.toArray();
         Chart chart = new ChartBuilder().width(800).height(600).theme(StyleManager.ChartTheme.Matlab).build();
-        
+
         chart.addSeries("sin", xplot, y.toArray());
         chart.addSeries("pred", xplot, yPredicted);
-        Utils.saveChart(chart, "mlp");
+        Utils.saveChart(chart, "mlpnn");
     }
-
 }
