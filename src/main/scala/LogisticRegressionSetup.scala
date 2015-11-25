050732417065a30543608fb2c89916f4b240aa9b
Minh Ngo <nlminhtl@gmail.com>
2015-11-25 16:28:32 +0100

    Removing all scala code.

--- src/main/scala/LogisticRegressionSetup.scala
+++
@@ -1,4 +0,0 @@
-class LogisticRegressionSetup(val nFeatures: Int) {
-    def getWeights() = Vector(nFeatures)
-    def getBias() = Scalar()
-}
--- src/main/scala/MLPSetup.scala
+++
@@ -1,6 +0,0 @@
-class MLPSetup(val nFeatures: Int, val hiddenSize: Int) {
-    def getW() = Vector(hiddenSize)
-    def getB() = Scalar()
-    def getV() = Matrix(nFeatures, hiddenSize)
-    def getA() = Vector(hiddenSize)
-}
