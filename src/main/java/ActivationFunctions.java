3b2ded5dde65d96c6f06a7ff01a8cd2d02386446
Sander Lijbrink <sander.lijbrink@student.uva.nl>
2015-12-01 00:14:21 +0100

    Activation function

--- src/main/java/ActivationFunctions.java
+++ src/main/java/ActivationFunctions.java
@@ -1,19 +1,21 @@
-import org.apache.commons.math3.analysis.function.Sigmoid;
 
 /**
  * Created by sander on 30/11/15.
  */
 public class ActivationFunctions {
-    public interface ActivationFunction {
-        double value(double x);
-        double derivative(double y);
-    }
 
-    public class Sigmoid extends org.apache.commons.math3.analysis.function.Sigmoid implements ActivationFunction {
+    public static class Sigmoid extends org.apache.commons.math3.analysis.function.Sigmoid implements ActivationFunction {
         @Override
-        public double derivative(double y) {
-            return 0;
+        public double derivative(double sigmoidValue) {
+            return sigmoidValue * (1.0 - sigmoidValue);
         }
     }
 
+    public static class Tanh extends org.apache.commons.math3.analysis.function.Tanh implements ActivationFunction {
+        @Override
+        public double derivative(double tanhValue) {
+            return 1.0 - (tanhValue * tanhValue);
+        }
+    }
+    
 }
