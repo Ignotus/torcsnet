84353a46c2d300ce2c3a3bc960242c9000c6be8f
Sander Lijbrink <sander.lijbrink@student.uva.nl>
2015-12-03 00:51:59 +0100

    Extra variables for weight files

--- src/main/java/Configuration.java
+++ src/main/java/Configuration.java
@@ -27,15 +27,26 @@ public class Configuration {
 
     public static final boolean RECORD_DATA = Boolean.parseBoolean(Configuration.value("record_data", "false"));
     public static final String PROP_FILE = Configuration.value("properties",
-            "/home/ignotus/Development/torcsnet/torcs.properties");
+            "/Users/sander/GitHub/torcsnet/torcs.properties");
     public static final String OUTPUT_FILE = Configuration.value("output_file",
-            "/home/ignotus/Development/torcsnet/out.txt");
+            "/Users/sander/GitHub/torcsnet/out.txt");
 
     // For MLP training
     // Should contain *.csv files for NN training
     public static final String CSV_DIRECTORY = Configuration.value("csv_directory",
-            "/home/ignotus/Development/torcsnet/data/");
+            "/Users/sander/Downloads/without_noise");
     public static final int LINE_VALUES = Integer.parseInt(Configuration.value("line_values", "27"));
     public static final String WEIGHTS_FILE = Configuration.value("weights_file",
-            "/home/ignotus/Development/torcsnet/src/main/resources/memory/weights.dump");
+            "/Users/sander/GitHub/torcsnet/src/main/resources/memory/weights.dump");
+
+    // For training with 3 separate NNs
+    public static final String WEIGHTS_FILE_ACCEL = Configuration.value("weights_accel_file",
+            "/Users/sander/GitHub/torcsnet/src/main/resources/memory/weights_accel.dump");
+
+    public static final String WEIGHTS_FILE_STEERING = Configuration.value("weights_steering_file",
+            "/Users/sander/GitHub/torcsnet/src/main/resources/memory/weights_steering.dump");
+
+    public static final String WEIGHTS_FILE_BRAKING = Configuration.value("weights_braking_file",
+            "/Users/sander/GitHub/torcsnet/src/main/resources/memory/weights_braking.dump");
+
 }
